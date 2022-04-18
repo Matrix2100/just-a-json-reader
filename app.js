aeca0ba375731f0d734a7d134e794ed0efb7c748
Vinicius Oliveira Gouveia <42838209+Matrix2100@users.noreply.github.com>
2022-04-18 13:33:46 -0300

    aa

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ app.listen(port);
 
 app.post('/api/', function (req, res) {
     // print to console
-    // console.log(util.inspect(req.body, {showHidden: false, depth: null, colors: true}))
-    console.log(req.body, {showHidden: false, depth: null, colors: true})
+    console.log(util.inspect(req.body, {showHidden: false, depth: null, colors: true}))
     // just call res.end(), or show as string on web
     res.send(req.body);
 });
