cd299b052c40098dc1dcd2a22f058f3df22309e0
Vinicius Oliveira Gouveia <42838209+Matrix2100@users.noreply.github.com>
2022-04-18 13:39:32 -0300

    a

--- app.js
+++ app.js
@@ -8,13 +8,25 @@ app.listen(port);
 
 app.post('/api/', function (req, res) {
     // print to console
-    console.log(util.inspect(req.body, {showHidden: false, depth: null, colors: true}))
+    printJsonRecusively(req.body);
     // just call res.end(), or show as string on web
     res.send(req.body);
 });
 
+function printJsonRecusively(obj){
+    let body = '';
+    printJsonRecusively(obj, body);
+    console.log(body);
+}
 
-// function printRecursivo(objeto) {
-//    cons
-// }
+function printJsonRecusively(obj, strg) {
+    if (typeof obj === 'object') {
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                strg += util.format('%s: %s\n', key, obj[key]);
+                printJsonRecusively(obj[key]);
+            } 
+        }
+    }
+}
 
