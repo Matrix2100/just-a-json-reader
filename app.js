05b51cdb5e9e49c230e237d7c8243eac1dd65600
Vinicius Oliveira Gouveia <42838209+Matrix2100@users.noreply.github.com>
2022-04-18 12:56:59 -0300

    correção json

--- app.js
+++ app.js
@@ -2,18 +2,13 @@ var express = require('express'),
     app     = express(),
     port    = 3333;
 
-var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
 app.listen(port);
 
-
-app.post('/api/', jsonParser, function (req, res) {
+app.post('/api/', function (req, res) {
     // print to console
     console.log(req.body);
 
     // just call res.end(), or show as string on web
-    res.send(JSON.stringify(req.body, null, 4));
+    res.send(JSON.stringify(req.body));
 });
 
