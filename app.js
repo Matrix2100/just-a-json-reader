6cce08a10602092b036d4493e141f26423d1e992
Vinicius Oliveira Gouveia <42838209+Matrix2100@users.noreply.github.com>
2022-04-18 13:12:29 -0300

    commit

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ var express = require('express'),
     app     = express(),
     port    = 3333;
 
+const util = require('util')
+
 app.listen(port);
 
 app.post('/api/', function (req, res) {
     // print to console
-    console.log(req.body);
+    console.log(util.inspect(myObject, {showHidden: false, depth: null, colors: true}))
 
     // just call res.end(), or show as string on web
-    res.send(JSON.stringify(req.body));
+    res.send(req.body);
 });
+
+
 
