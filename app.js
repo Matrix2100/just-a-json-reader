a44a51729379104cb6eb022b3e7f52a01938837c
Vinicius Oliveira Gouveia <vinicius.gouveia>
2022-04-19 12:34:31 -0300

    iha

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
 var express = require('express'),
     app     = express(),
-    port    = 3333;
+    port    = 3333
 
 const util = require('util')
 
-app.listen(port);
+app.use(express.json())
+
+app.listen(port)
 
 app.post('/api/', function (req, res) {
     // print to console
-    printJsonRecusively(req.body);
+    // console.log(req.body);
+    printJsonRecusively(req.body)
     // just call res.end(), or show as string on web
-    res.send(req.body);
+    res.send("req.boAdy")
 });
 
-function printJsonRecusively(obj){
-    let body = 'e';
-    printJsonRecusively(obj, body);
-    console.log(body);
+function printJsonRecusively(obj) {
+    var text = printJsonRecusively2(obj, '')
+    console.log(text)
+    console.log(" \n")
 }
 
-function printJsonRecusively(obj, strg) {
-    if (typeof obj === 'object') {
-        for (var key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                strg += util.format('%s: %s\n', key, obj[key]);
-                printJsonRecusively(obj[key]);
-            } 
+function printJsonRecusively2(obj, text) {
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            if (typeof obj[key] == "object") {
+                text +=  key + ": "
+                // console.log(key)
+                text = printJsonRecusively2(obj[key], text)
+            } else {
+                text += "\n" + key + ": " + obj[key] + ", "
+                // console.log(key + " -> " + obj[key])
+            }
         }
     }
-}
-
+    return text
+} 
